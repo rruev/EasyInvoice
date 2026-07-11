53e672ceac9a9be1a083562e773a228e0b6f0053
rruev <rosenruev@gmail.com>
2026-07-11 16:26:08 +0200

    conect register controller with register service

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,15 +1,20 @@
 import { Router } from 'express';
+import authService from '../services/auth.service';
 
 const authController = Router();
 
-authController.post('/login', (req, res) => {
-  // Handle login logic here
-  res.send('Login endpoint');
+authController.post('/register', async (req, res) => {
+    const userData = req.body;
+
+    const user = await authService.register(userData);
+    res.json(user);
 });
 
-authController.post('/register', (req, res) => {
-  // Handle registration logic here
-  res.send('Register endpoint');
+authController.post('/login', async (req, res) => {
+    const userData = req.body;
+
+    const user = await authService.login(userData);
+    res.json(user);
 });
 
 export default authController;
---
+++ backend/src/services/auth.service.js
@@ -0,0 +1,14 @@
+const register = async (userData) => {
+    return userData;
+}
+
+const login = async (userData) => {
+    return userData;
+}
+
+const authService = {
+    login,
+    register
+};
+
+export default authService;
